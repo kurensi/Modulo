b5d6d6c9a0b4b2e79bf8c70f5797b33f3b7ce5ac
dejan <dejan@DESKTOP-HA22HU0>
2023-02-28 18:08:00 +0100

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿try
+{
+    Console.Write("die zahl x:");
+    int x = Convert.ToInt32(Console.ReadLine());
+
+    if (x % 3 == 0)
+    {
+        Console.WriteLine($"{x} ist dividierbar durch 3 oder 10");
+    }
+    else if (x % 9 == 0)
+    {
+        Console.WriteLine($"{x} ist durch 3 und 9 teilbar");
+    }
+    else
+    {
+        Console.WriteLine($" ist nicht durch 9 oder 3 teilbar");
+    }
+}
+
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+}
+Console.ReadLine();
